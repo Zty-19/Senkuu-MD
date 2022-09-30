0992c8a0188f179285d2fc8cf962cc84579d5b1b
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-09-30 09:52:46 +0700

    Update source.js

--- command/info/source.js
+++ command/info/source.js
@@ -5,10 +5,10 @@ module.exports = {
   alias: ["sc","source"],
   category: "info",
   async run({msg, conn}){
-    ss = await far.tools.ssweb("https://github.com/xzeera-id/Senkuu-MD")
+    ss = await far.tools.ssweb("api.faruqofc.xyz")
     repo = await sc.ghrepo("Senkuu-MD")
     txt = "*乂 S O U R C E - B O T*\n\n"
-    txt += ` • *SC : https://github.com/xzeera-id/Senkuu-MD*\n`
+    txt += ` • *SC : wa.me/6285807264974*\n`
     txt += ` • *Watch : ${repo.items[0].watchers}*\n`
     txt += ` • *Forks : ${repo.items[0].forks}*\n`
     txt += ` • *Language : ${repo.items[0].language}*\n`
